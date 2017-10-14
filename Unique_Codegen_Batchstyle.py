b28a7620b697c070467d4d86a4f4c07a6cdc00ac
192Iridium77 <matt.martin0108@live.com>
2017-10-14 14:24:09 +1030

    created batching capability

--- Unique_Codegen_Batchstyle.py
+++ Unique_Codegen_Batchstyle.py
@@ -1,21 +1,25 @@
 # Generation of 6 alphanum characters gives 1.838 billion possible unique characters
 # You could also generate ~60 million unique characters using numeric
-# use batchstyle for separation of code batches
 
 from numpy import random
 from datetime import *
 
-def generate_codestring(code_length):
+
+def generate_codestring(code_length, batch_dict):
     codestring = ''
     i = 0
     while i < code_length:
-        alphanum_select = random.randint(0, 36)
-        if alphanum_select <= 9:
-            codestring += str(alphanum_select)
+        if i in batch_dict.keys():
+            codestring += batch_dict[i]
+            i += 1
         else:
-            # 10 = a, but we want A, which is 65, therefore add 55
-            codestring += chr(alphanum_select + 55)
-        i += 1
+            alphanum_select = random.randint(0, 36)
+            if alphanum_select <= 9:
+                codestring += str(alphanum_select)
+            else:
+                # 10 = a, but we want A, which is 65, therefore add 55
+                codestring += chr(alphanum_select + 55)
+            i += 1
 
     return codestring
 
@@ -26,13 +30,23 @@ def check_uniqueness(code, lst):
     return True
 
 
+# batch_sig is in format **C*8*, either numeric or uppercase alpha
+def read_batch_signature(batch_sig):
+    batch_dict = {}
+    for i in range(len(batch_sig)):
+        if batch_sig[i].isalpha():
+            batch_dict[i] = batch_sig[i]
+    return batch_dict
+
+
 def main():
     codelist = []
     num_characters = eval(input("Enter number of characters: "))
     num_codes = eval(input("Enter number of codes to generate: "))
+    batch_signature = str(input("Enter batch code, eg. *B*5**: "))
     start_time = datetime.now()
     for i in range(num_codes):
-        new_code = generate_codestring(num_characters)
+        new_code = generate_codestring(num_characters, read_batch_signature(batch_signature))
         if check_uniqueness(new_code, codelist):
             codelist.append(new_code)
     print("Done.")
